b7ef699138d0fb37d93553ea92069fec34b3d43c
unknown <kieledssh@gmail.com>
2022-08-24 14:36:12 +0300

    All done

---
+++ src/components/AddCurrency.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AddCurrency"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/CurrencyItem.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CurrencyItem"
+}
+</script>
+
+<style scoped>
+
+</style>
